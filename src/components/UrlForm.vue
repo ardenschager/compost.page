8f4cbedbcbab9fe52e081b9fda520f1d4932ecd3
nsx0x <nathanschager@gmail.com>
2023-01-10 23:25:14 -0800

    changed suffering link

--- src/components/UrlForm.vue
+++ src/components/UrlForm.vue
@@ -45,6 +45,7 @@ function getOptions() {
                 'archives.gov/research/military/air-force/ufos',
                 'cameronsworld.net', 
                 'foxnews.com',
+                'neoseeker.com/suffering/',
                 'nytimes.com', 
                 'pastebin.com/FxKf1Xnz',
                 'taxi1010.com/index0.htm', 
@@ -52,7 +53,6 @@ function getOptions() {
                 'timecube.2enp.com/',
                 'tinyurl.com/50-affirmations',
                 'tinyurl.com/sexual-dimorphism',
-                'tinyurl.com/the-suffering',
                 'www.yyyyyyy.info/'
             ];
 }
